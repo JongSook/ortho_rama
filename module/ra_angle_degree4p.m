872a94e0137796d1497d2333c96e8aa86c73d3d4
Jongsook San <jongsook.san@gmail.com>
2022-06-15 10:43:44 +0700

    Add files via upload

--- module/ra_angle_degree4p.m
+++ module/ra_angle_degree4p.m
@@ -1,7 +1,7 @@
-function ang_deg = angle_degree3p(point1, point2, point_innitial)
+function ang_deg = ra_angle_degree3p(point1, point2, point3, point4)
 
-    AB = point1 - point_innitial;
-    CD = point2 - point_innitial;
+    AB = point1 - point2;
+    CD = point3 - point4;
     ABCDdot = AB(1)*CD(1) + AB(2)*CD(2) + AB(3)*CD(3);
     ABabs = abs(sqrt(AB(1)*AB(1) + AB(2)*AB(2) + AB(3)*AB(3)));
     CDabs = abs(sqrt(CD(1)*CD(1) + CD(2)*CD(2) + CD(3)*CD(3)));
@@ -9,4 +9,3 @@ function ang_deg = angle_degree3p(point1, point2, point_innitial)
     ang_deg = acosd(ABCDfnv);
     
 end
-
